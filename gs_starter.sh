229bb4ff13cb8ed25f10af142f4528256a31b425
Paul Klumpp <paul@klumpp.net>
2012-11-16 22:28:11 +0100

    license again

--- gs_starter.sh
+++ gs_starter.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+# gs_starter.sh - Tool for managing unix screens with bash. Typically game servers.
+# ----------------------------------------------------------------
+# Copyright (C) 2012 Paul-Dieter Klumpp
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------
 # vim: expandtab tabstop=4 shiftwidth=4 softtabstop=4 autoindent:
 #
 # bash is needed!!
